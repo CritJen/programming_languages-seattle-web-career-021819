519737e4c53bbb674ca9105ce8316d3367fd8d94
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 04:44:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,8 +5,9 @@ def reformat_languages(languages)
     language_stuff.each do |language, type_hash|
       new_hash[language] = {}
       new_hash[language][:type] = type_hash[:type]
+      binding.pry
       if new_hash[language].key?("style")
-      
+
           new_hash[language][:style] << style_of_language
       else
             new_hash[language][:style] = []
