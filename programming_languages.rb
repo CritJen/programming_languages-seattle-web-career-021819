458f5affcfc1947ab17631806553a7372782b4b3
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 03:53:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style_of_language, language|
+    new_hash[language] = {style: style_of_language}
+    language.each do |type, attribute|
+      new_hash[language] = {type: attribute}
+      binding.pry
+    end
+  end
+  puts new_hash
 end
