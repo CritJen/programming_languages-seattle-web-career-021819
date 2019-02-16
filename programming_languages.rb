fda0802ab17c89fd78e621ec7af67880a01fd7e7
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-16 04:23:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,6 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style_of_language, language_stuff|
     language_stuff.each do |language, type_hash|
-      binding.pry
       new_hash[language] = type_hash
       new_hash[language][:style] = style_of_language
     end
